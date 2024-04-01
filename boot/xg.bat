d76ac1f68153dafc730db601b197d976bd53221b
dishuo <dishuo183@outlook.com>
2024-04-01 19:54:51 +0800

    push

---
+++ boot/xg.bat
@@ -0,0 +1,7 @@
+@echo off & setlocal enabledelayedexpansion
+set "bb=D:\ProgramFiles\Desktop\tools\DSPB\TOOL\bin\busybox.exe"
+for /f %%a in ('!bb! ls !cd! ^|!bb! grep "A"') do (
+!bb! sed -i "s/DS_//g" !cd!\%%a
+!bb! sed -i "s/_%%a//g" !cd!\%%a
+!bb! sed -i "s/.7z /=/g" !cd!\%%a )
+pause & exit
